3e58647a94cd95d1eaff116ba2fbf7520858e4e8
Mallumovie7 <120130608+Mallumovie7@users.noreply.github.com>
2022-12-13 18:01:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tanujairam-TG/Emilia-clarke.git /Emilia-clarke
+  git clone https://github.com/Mallumovie7/Emilia-clarkee.git /Emilia-clarke
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Emilia-clarke
@@ -10,3 +10,4 @@ cd /Emilia-clarke
 pip3 install -U -r requirements.txt
 echo "𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐄𝐦𝐢𝐥𝐢𝐚 𝐜𝐥𝐚𝐫𝐤𝐞 🔥🔥🔥..."
 python3 bot.py
+
